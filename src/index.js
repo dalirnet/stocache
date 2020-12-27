bd0312f2ee747616deffb3bd04f8d38f6f18008e
AmirRezaDalir <dalirnet@gmail.com>
2020-12-27 14:26:44 +0330

    middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-// perfix
-const PERFIX = 'stocache'
+const config = {
+    scope: 'stocache',
+    ttl: 3600,
+    exception: false,
+}
 
-// support
 const support = ((key) => {
     try {
         localStorage.setItem(key, JSON.stringify(true))
@@ -9,76 +11,109 @@ const support = ((key) => {
     } catch (e) {
         return false
     }
-})(PERFIX + '-' + 'support')
+})(config.scope + '-' + 'support')
 
-// json
-const json = (value) => ({
-    encode: (fallback = '') => {
-        try {
-            return JSON.stringify(value)
-        } catch (e) {
-            return fallback
-        }
-    },
-    decode: (fallback = {}) => {
-        try {
-            return JSON.parse(value)
-        } catch (e) {
-            return fallback
-        }
-    },
-})
+const Stocache = (scope = config.scope, exception = config.exception) => {
+    const generator = (scope) => ({
+        key: (value = scope) => {
+            let code = 0
+            for (let i = 0; i < value.length; i++) {
+                code = (code << 5) - code + value.charCodeAt(i)
+                code = code & code
+            }
+            return scope + '-' + (code > 0 ? code : 'i' + Math.abs(code))
+        },
+        exception: (name = null, message = null) => ({
+            scope,
+            name,
+            message,
+        }),
+    })
 
-// generator
-const generator = (perfix = PERFIX) => ({
-    key: (value = perfix) => {
-        let code = 0
-        for (let i = 0; i < value.length; i++) {
-            code = (code << 5) - code + value.charCodeAt(i)
-            code = code & code
-        }
-        return perfix + '-' + (code > 0 ? code : 'i' + Math.abs(code))
-    },
-})
+    const json = (value) => ({
+        encode: (fallback = '') => {
+            try {
+                return JSON.stringify(value)
+            } catch ({ message }) {
+                if (exception) throw generator(scope).exception('json.encode', message)
+                return fallback
+            }
+        },
+        decode: (fallback = {}) => {
+            try {
+                return JSON.parse(value)
+            } catch ({ message }) {
+                if (exception) throw generator(scope).exception('json.decode', message)
+                return fallback
+            }
+        },
+    })
 
-// stocache
-const stocache = (perfix = PERFIX) => {
-    const storage = (key = perfix) => {
-        const set = (value = null, retry = false) => {
+    const storage = (key = scope) => ({
+        set: (value = null, retry = false) => {
             try {
                 localStorage.setItem(key, json(value).encode())
                 return true
             } catch ({ message }) {
+                if (exception) throw generator(scope).exception('storage.set', message)
                 return false
             }
+        },
+        unset: () => {
+            try {
+                localStorage.removeItem(key)
+                return true
+            } catch ({ message }) {
+                if (exception) throw generator(scope).exception('storage.unset', message)
+                return false
+            }
+        },
+        get: (fallback = false) => {
+            return json(localStorage.getItem(key)).decode(fallback)
+        },
+    })
+
+    const middleware = (key, now = Math.floor(Date.now() / 1000)) => {
+        let keys = {
+            shadow: generator(scope).key(),
+            storage: generator(scope).key(key),
         }
-
-        const get = (fallback = false) => {
-            return json(localStorage.getItem(key) || fallback).decode(fallback)
-        }
-
-        return { set, get }
-    }
-
-    const shadow = (key = perfix) => {
-        const now = Math.floor(Date.now() / 1000)
-
-        const keys = {
-            shadow: generator(perfix).key(),
-            storage: generator(perfix).key(key),
-        }
-
-        let context = storage(keys.shadow).get({})
+        let context = storage(keys.shadow).get() || {}
 
         const set = (value, ttl) => {
-            if (storage(keys.storage).set(value, true)) {
-                return keep(ttl, true)
+            if (support) {
+                if (scope !== key) {
+                    if (storage(keys.storage).set(value, true)) {
+                        return keep(ttl, true)
+                    }
+                } else {
+                    if (exception) throw generator(scope).exception('middleware.set', 'KeyConflict')
+                }
             }
             return false
         }
 
-        const keep = (ttl, condition = context[keys.storage]) => {
-            if (condition) {
+        const unset = () => {
+            if (support) {
+                delete context[keys.storage]
+                return storage(keys.storage).unset() && storage(keys.shadow).set(context)
+            }
+            return false
+        }
+
+        const has = () => {
+            if (support && context[keys.storage]) {
+                if (context[keys.storage] >= now) {
+                    return true
+                } else {
+                    unset()
+                }
+            }
+            return false
+        }
+
+        const keep = (ttl, condition = has(keys.storage)) => {
+            if (support && condition) {
                 context[keys.storage] = now + parseInt(ttl)
                 return storage(keys.shadow).set(context)
             }
@@ -86,19 +121,24 @@ const stocache = (perfix = PERFIX) => {
         }
 
         const get = (fallback) => {
-            // console.log(map[keys.storage])
-            return storage(keys.storage).get(fallback)
+            if (support && has(keys.storage)) {
+                return storage(keys.storage).get(fallback)
+            }
+            return fallback
         }
 
-        return { set, keep, get }
+        return { set, unset, has, keep, get }
     }
 
     return {
         support,
-        set: (key, value = null, ttl = 300) => (support ? shadow(key).set(value, ttl) : false),
-        keep: (key, ttl = 300) => (support ? shadow(key).keep(ttl) : false),
-        get: (key, fallback = false) => (support ? shadow(key).get(fallback) : fallback),
+        key: (key = config.scope) => generator(scope).key(key),
+        set: (key = null, value = null, ttl = config.ttl) => middleware(key).set(value, ttl),
+        unset: (key = null) => middleware(key).unset(),
+        has: (key = null) => middleware(key).has(),
+        keep: (key = null, ttl = config.ttl) => middleware(key).keep(ttl),
+        get: (key = null, fallback = false) => middleware(key).get(fallback),
     }
 }
 
-export default stocache
+export default Stocache
